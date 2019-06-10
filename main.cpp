543cd905316c7ecc3bd03b53c65da07026d95339
Ofir Shechtman <ofir511996@gmail.com>
2019-06-10 13:30:03 +0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Rational.h"
-
-int main(){
-    Rational r1(1);
-    Rational r2(1,2);
-    std::cout << r1+r2 << std::endl;
-    std::cout << (double)(r1+r2) << std::endl;
-}
